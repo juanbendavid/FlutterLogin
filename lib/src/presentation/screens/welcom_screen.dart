9f08f4774060d9b31094918e45a64bef1e74b184
juanbendavid <56058007+juanbendavid@users.noreply.github.com>
2023-08-30 20:31:09 -0400

    animations + texts

--- lib/src/presentation/screens/welcom_screen.dart
+++ lib/src/presentation/screens/welcom_screen.dart
@@ -1,19 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Column(
+      body: _CustomBody(),
+    );
+  }
+}
+
+class _CustomBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size.height;
+    return Column(
+      children: [
+        _AnimationWidget(size: size),
+        _WelcomeWidget(size: size),
+      ],
+    );
+  }
+}
+
+class _WelcomeWidget extends StatelessWidget {
+  const _WelcomeWidget({
+    required this.size,
+  });
+
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 60),
+      height: size * 0.4,
+      width: double.infinity,
+      decoration: const BoxDecoration(
+          color: Colors.amber,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(38), topRight: Radius.circular(38))),
+      child: const Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            height: size * 0.6,
-            width: double.infinity,
-            color: Colors.amber,
-            child: Text("data"),
+          Text(
+            "Welcome",
+            style: TextStyle(fontWeight: FontWeight.w800, fontSize: 35),
+          ),
+          Text(
+            "Voluptate dolore irure est eiusmod quis qui proident tempor.",
+            style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _AnimationWidget extends StatelessWidget {
+  const _AnimationWidget({
+    required this.size,
+  });
+
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: size * 0.6,
+      width: double.infinity,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Lottie.asset('assets/animations/beer.json'),
+          ),
+          const Text(
+            "Deeps",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+          ),
+          const Text(
+            "B E E R C A F E",
+            style: TextStyle(fontWeight: FontWeight.w300, fontSize: 25),
           )
         ],
       ),
